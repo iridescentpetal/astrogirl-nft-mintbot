a7ebf1a82c4a3f080412005e71dfbcf3126b3077
Daria Jung <dariajung@Darias-MacBook-Pro.local>
2022-03-20 15:19:03 -0400

    validate logic to download astrogirls

--- index.ts
+++ index.ts
@@ -1,6 +1,7 @@
 require("dotenv").config();
 
 import listenAstroMint from "./lib/eth/listenMint"
+import downloadImage from "./lib/bot/downloadAstrogirl"
 
 async function go() {
   await listenAstroMint();
--- lib/eth/listenMint.ts
+++ lib/eth/listenMint.ts
@@ -1,9 +1,9 @@
 import { ASTRO_CONTRACT_ADDR, NULL_ADDRESS, astroContract } from "./contract";
 
 async function listenAstroMint(/*callback: (address: string) => void*/) {
-    console.log("Listening for mint events for new Astro Girls!");
+    console.log("Listening to transfer events for Astro Girls!");
     astroContract.on("Transfer", (from, to, tokenId, event) => {
-        console.log("Transfer event at", new Date(), ". From:", from, "to:", to);
+        console.log("Transfer event at", new Date(), "| From:", from, "to:", to);
     })
     
     const filter = astroContract.filters.Transfer(NULL_ADDRESS);
