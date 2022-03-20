71ffb2bf6be796171a71df2a993fe168de8f82d3
Daria Jung <dariajung@Darias-MacBook-Pro.local>
2022-03-19 21:47:42 -0400

    lol remove interval

--- index.ts
+++ index.ts
@@ -4,7 +4,6 @@ import listenAstroMint from "./lib/eth/listenMint"
 
 async function go() {
   await listenAstroMint();
-  setInterval(() => listenAstroMint(), /* 30 seconds =*/ 1000 * 5);
 }
 
 go();
