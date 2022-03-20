eeb4f0f33972ccd73decced7a846c91922ef4ad2
Daria Jung <dariajung@Darias-MacBook-Pro.local>
2022-03-20 14:03:53 -0400

    add twitter oauth logic

--- lib/eth/contract/index.ts
+++ lib/eth/contract/index.ts
@@ -5,4 +5,6 @@ import astroAbi from "./abi/astro"
 
 export const ASTRO_CONTRACT_ADDR = "0xB21e760e7f74dD3c7b125EF21ba068AbBC8ADBA8";
 export const NULL_ADDRESS = "0x0000000000000000000000000000000000000000";
+// Concatenate with tokenID and .png suffix to get the image of the Astro Girl
+export const ASTRO_GIRL_IMG_PREFIX = "https://gateway.pinata.cloud/ipfs/QmPZuVJmwtHeTZVXzatQ36dR3k8RzmDcB6DA5BnYnAHXLh/";
 export const astroContract = new Contract(ASTRO_CONTRACT_ADDR, astroAbi, provider);
