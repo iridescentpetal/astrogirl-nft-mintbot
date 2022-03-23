b9f09b6f0ff85e261a36f7ab6ae22f4fc79c3820
iridescentpetal <salaryslut@gmail.com>
2022-03-22 20:18:01 -0400

    add console log

--- lib/eth/listenMint.ts
+++ lib/eth/listenMint.ts
@@ -1,6 +1,7 @@
 import { ASTRO_CONTRACT_ADDR,  astroContract } from "./contract";
 
 async function listenAstroMint(/*callback: (address: string) => void*/) {
+    console.log("Listening for mint events for new Astro Girls!");
     astroContract.on("Transfer", (from, to, tokenId, event) => {
         console.log("Is this working?")
     })
