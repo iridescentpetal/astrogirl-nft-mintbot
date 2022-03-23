8d065cf9091123ccac4ab14f7e4a6c14429c9912
iridescentpetal <salaryslut@gmail.com>
2022-03-22 20:18:01 -0400

    get discord logic working

--- lib/bot/createTwitterClient.ts
+++
@@ -1,7 +0,0 @@
-import { TwitterApi } from "twitter-api-v2";
-
-function createTwitterClient() {
-    return new TwitterApi(process.env.TWITTER_API_BEARER_TOKEN);
-}
-
-export default createTwitterClient;
