da143bfc15f936b0cee058cb1344410b8c1a4964
iridescentpetal <salaryslut@gmail.com>
2022-03-22 20:18:01 -0400

    restructure directories

---
+++ index.ts
@@ -0,0 +1,10 @@
+require("dotenv").config();
+
+import listenAstroMint from "./lib/eth/listenMint"
+
+async function go() {
+  await listenAstroMint();
+  setInterval(() => listenAstroMint(), /* 30 seconds =*/ 1000 * 30);
+}
+
+go();
