7d63c721a8f11db2ac865335015ba0bec15d7c29
iridescentpetal <salaryslut@gmail.com>
2022-03-22 20:18:01 -0400

    consolidate log statements and remove if statement to mimic filtering now that filtering works

--- lib/eth/listenMint.ts
+++ lib/eth/listenMint.ts
@@ -3,21 +3,12 @@ import { ASTRO_CONTRACT_ADDR, NULL_ADDRESS, astroContract } from "./contract";
 async function listenAstroMint(/*callback: (address: string) => void*/) {
     console.log("Listening for mint events for new Astro Girls!");
     astroContract.on("Transfer", (from, to, tokenId, event) => {
-        console.log("Transfer event at", new Date());
-        console.log("From address:", from, "To address:", to);
-        console.log("TokenId:", tokenId);
-
-        // Janky if statement to mimic filtering.
-        if (from == NULL_ADDRESS) {
-            console.log(`Mint event, Astro Girl #${ tokenId } minted.`);
-        }
+        console.log("Transfer event at", new Date(), ". From:", from, "to:", to);
     })
     
     const filter = astroContract.filters.Transfer(NULL_ADDRESS);
-    
     astroContract.on(filter, (from, to, tokenId, event) => {
-        console.log(`Mint event, Astro Girl #${ tokenId } minted.`);
-        console.log("Minted at ", new Date());
+        console.log(`Astro Girl #${ tokenId } minted at`, new Date());
     });
 }
 
