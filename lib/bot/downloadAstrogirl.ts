24ac694156c2092db54c837f1130012dd0ea90fc
iridescentpetal <salaryslut@gmail.com>
2022-03-22 20:18:01 -0400

    add logic to download astro girl img

---
+++ lib/bot/downloadAstrogirl.ts
@@ -0,0 +1,20 @@
+import { createWriteStream } from 'fs';
+import { get } from 'https';
+
+function downloadImage(url: string, filepath: string) {
+    return new Promise((resolve, reject) => {
+        get(url, (res) => {
+            if (res.statusCode === 200) {
+                res.pipe(createWriteStream(filepath))
+                    .on('error', reject)
+                    .once('close', () => resolve(filepath));
+            } else {
+                // Consume response data to free up memory
+                res.resume();
+                reject(new Error(`Request Failed With a Status Code: ${res.statusCode}`));
+            }
+        });
+    });
+}
+
+export default downloadImage;
