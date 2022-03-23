a332b6402c33a6c70d70b0976ec30d08624ce731
iridescentpetal <salaryslut@gmail.com>
2022-03-22 20:18:01 -0400

    add timestamp to logging inside transfer event

--- lib/eth/listenMint.ts
+++ lib/eth/listenMint.ts
@@ -3,13 +3,14 @@ import { ASTRO_CONTRACT_ADDR,  astroContract } from "./contract";
 async function listenAstroMint(/*callback: (address: string) => void*/) {
     console.log("Listening for mint events for new Astro Girls!");
     astroContract.on("Transfer", (from, to, tokenId, event) => {
-        console.log("Is this working?")
+        console.log("Transfer event at ", new Date());
     })
     
     const filter = astroContract.filters.Transfer(ASTRO_CONTRACT_ADDR, null);
     
     astroContract.on(filter, (from, to, tokenId, event) => {
         console.log(`Mint event, Astro Girl #${ tokenId } minted by wallet ${ to }.`);
+        console.log("Minted at ", new Date());
     });
 }
 
